df14794b81994d1049b0a7f338bed87e6eaad526
symnrtzl <symnrtzl99@gmail.com>
2023-09-25 01:53:57 +0300

    Mathematics

---
+++ Mathematics/FourOperation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathematics
+{
+    class FourOperation
+    {
+        public void Addition(int number1, int number2) {
+            int result = number1 + number2;
+            Console.WriteLine("Total : " + result);
+        }
+    }
+}
---
+++ Mathematics/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Mathematics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FourOperation fourOperation = new FourOperation();
+            fourOperation.Addition(2,4);
+        }
+    }
+}
