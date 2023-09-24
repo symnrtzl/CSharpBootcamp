2479de04f771fb2b53f57d7f797396e5e4ca26d3
symnrtzl <symnrtzl99@gmail.com>
2023-09-25 01:50:21 +0300

    Dictionary
    DictionaryDemo

---
+++ DictionaryDemo/Dictionary.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryDemo
+{
+    class Dictionary<Tkey, Tvalue>
+    {
+        Tkey[] keys;
+        Tvalue[] values;
+        public Dictionary()
+        {
+            keys = new Tkey[0];
+            values = new Tvalue[0];
+        }
+
+        public void Add(Tkey key, Tvalue value)
+        {
+            Tkey[] tempKeys = keys;
+            Tvalue[] tempValues = values;
+
+            keys = new Tkey[tempKeys.Length + 1];
+            values = new Tvalue[tempValues.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+            }
+            keys[keys.Length - 1] = key;
+
+            for (int i = 0; i < tempValues.Length; i++)
+            {
+                values[i] = tempValues[i];
+            }
+            values[keys.Length - 1] = value;
+        }
+
+
+        public int ValuesLength
+        {
+            get { return values.Length; }
+        }
+        public int KeysLength
+        {
+            get { return values.Length; }
+        }
+
+        public Tkey[] Keys
+        {
+            get { return keys; }
+        }
+
+        public Tvalue[] Values
+        {
+            get { return values; }
+        }
+
+        
+    }
+}
---
+++ DictionaryDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace DictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] strings)
+        {
+            Dictionary<String,String> dictionarys = new Dictionary<String, String>();
+            dictionarys.Add("Selim", "Selim");
+            dictionarys.Add("Pelin", "Kaya");
+            Console.WriteLine(dictionarys.ValuesLength);
+
+            dictionarys.Add("Kaan", "Kurt");
+            dictionarys.Add("Ali", "Dere");
+            Console.WriteLine(dictionarys.ValuesLength);
+
+            foreach (var dictionary in dictionarys.Keys)
+            {
+                Console.WriteLine(dictionary);
+            }
+
+            foreach (var dictionary in dictionarys.Values)
+            {
+                Console.WriteLine(dictionary);
+            }
+
+        }
+    }
+}
